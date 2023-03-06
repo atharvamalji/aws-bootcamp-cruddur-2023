cf85a2f4962cdd6a862c362cb96179a84a985217
Atharva Malji <atharva.malji.2001@gmail.com>
2023-03-06 18:58:06 +0000

    week_2_update_01

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,7 +14,26 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# honeycomb stuff
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 app = Flask(__name__)
+
+# honecomb stuff
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
@@ -71,7 +90,6 @@ def data_home():
 def data_notifications():
   data = NotificationActivities.run()
   return data, 200
-
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 def data_handle(handle):
