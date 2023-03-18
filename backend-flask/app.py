3538ad396a0f78414313eae7320ae578ad724633
Atharva Malji <atharva.malji.2001@gmail.com>
2023-03-18 14:18:19 +0000

    disabled rollbar instrumentation.

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -46,12 +46,12 @@ from services.show_activity import *
 # LOGGER.addHandler(console_handler)
 # LOGGER.addHandler(cw_handler)
 
-# rollbar
-import rollbar
-import rollbar.contrib.flask
-from flask import got_request_exception
+# # rollbar
+# import rollbar
+# import rollbar.contrib.flask
+# from flask import got_request_exception
 
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 
 
 app = Flask(__name__)
@@ -76,28 +76,29 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
-# rollbar
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'production',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
+# # rollbar
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(
+#         # access token
+#         rollbar_access_token,
+#         # environment name
+#         'production',
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False)
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
-@app.after_request
-def after_request(response):
-    timestamp = strftime('[%Y-%b-%d %H:%M]')
-    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-    return response
+# # cloudwatch
+# @app.after_request
+# def after_request(response):
+#     timestamp = strftime('[%Y-%b-%d %H:%M]')
+#     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#     return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -196,11 +197,11 @@ def data_activities_reply(activity_uuid):
     return model['data'], 200
   return
 
-# rollbar
-@app.route('/rollbar/test')
-def rollbar_test():
-    rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
+# # rollbar
+# @app.route('/rollbar/test')
+# def rollbar_test():
+#     rollbar.report_message('Hello World!', 'warning')
+#     return "Hello World!"
 
 if __name__ == "__main__":
   app.run(debug=True)
